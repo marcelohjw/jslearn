521a6a64c3557e72327fc3bec9c9464b73055af2
Marcelo Junior <marcelohjw@gmail.com>
2022-03-26 19:14:18 -0300

    Arrow functions done

--- play.js
+++ play.js
@@ -3,7 +3,7 @@ var age = 25;
 var hasHobbies = true;
 let exc = '!'
 
-function sumerizeUser(userName, userAge, userHasHobbies) {
+const sumerizeUser = (userName, userAge, userHasHobbies) => {
     if (userHasHobbies == true) {
         userHasHobbies = 'Sim!';
     };
@@ -12,6 +12,6 @@ function sumerizeUser(userName, userAge, userHasHobbies) {
         'O nome é ' + userName
     + ', a idade é ' + userAge
     + ' e ' + userName + ' gosta de alguma atividade? ' + userHasHobbies);
-}
+};
 
 console.log(sumerizeUser(name, age, hasHobbies));
